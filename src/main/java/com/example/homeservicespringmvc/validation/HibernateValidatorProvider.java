43ffb64c9fa1b322b94eb2660e79afa391ad0796
Arashap4 <arashmohamadi.developer@gmail.com>
2023-01-04 17:04:24 +0330

    edit HibernateValidatorProvider class

--- src/main/java/com/example/homeservicespringmvc/validation/HibernateValidatorProvider.java
+++ src/main/java/com/example/homeservicespringmvc/validation/HibernateValidatorProvider.java
@@ -1,9 +1,11 @@
 package com.example.homeservicespringmvc.validation;
 
 import com.example.homeservicespringmvc.entity.BaseEntity;
-import com.example.homeservicespringmvc.exception.CustomizedIllegalArgumentException;
+import com.example.homeservicespringmvc.exception.CustomizedHibernateValidatorProviderException;
 import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
 import jakarta.validation.ValidatorFactory;
+
 import java.util.Set;
 
 public class HibernateValidatorProvider {
@@ -11,18 +13,14 @@ public class HibernateValidatorProvider {
     private static final ValidatorFactory validatorFactory;
 
     static {
-        validatorFactory = jakarta.validation.Validation.buildDefaultValidatorFactory();
-    }
-
-    public static ValidatorFactory getValidatorFactory() {
-        return validatorFactory;
+        validatorFactory = Validation.buildDefaultValidatorFactory();
     }
 
     public static void checkEntity(BaseEntity<Long> entity) {
-        Set<ConstraintViolation<BaseEntity<Long>>> constraintViolations = getValidatorFactory()
+        Set<ConstraintViolation<BaseEntity<Long>>> constraintViolations = validatorFactory
                 .getValidator()
                 .validate(entity);
         if (!constraintViolations.isEmpty())
-            throw new CustomizedIllegalArgumentException(constraintViolations.toString());
+            throw new CustomizedHibernateValidatorProviderException(constraintViolations.toString());
     }
 }
