130572b963f918110c0b7dad4d30f74705c771cb
Arashap4 <arashmohamadi.developer@gmail.com>
2023-01-04 18:52:31 +0330

    create MainService class

---
+++ src/main/java/com/example/homeservicespringmvc/entity/capability/MainServices.java
@@ -0,0 +1,44 @@
+package com.example.homeservicespringmvc.entity.capability;
+
+import com.example.homeservicespringmvc.entity.users.Specialist;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+
+
+@Entity
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+public class MainServices extends BaseAbility {
+    @Column(unique = true)
+    @NotBlank(message = "name of service should not contains null value or space . ")
+    private String name;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "services", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<SubServices> subList;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "servicesSet")
+    private Set<Specialist> specialistSet;
+
+    public void addSpecialist(Specialist specialist) {
+        this.getSpecialistSet().add(specialist);
+        specialist.getServicesSet().add(this);
+    }
+
+    public void addSubServices(SubServices subServices) {
+        this.getSubList().add(subServices);
+        subServices.setServices(this);
+    }
+
+    @Builder
+    public MainServices(String name) {
+        this.name = name;
+    }
+}
