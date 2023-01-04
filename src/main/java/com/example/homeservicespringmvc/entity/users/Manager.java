8345ccac5436e35dfc03b5e0839796983d467a26
Arashap4 <arashmohamadi.developer@gmail.com>
2023-01-04 18:09:33 +0330

    create Manager class .

---
+++ src/main/java/com/example/homeservicespringmvc/entity/users/Manager.java
@@ -0,0 +1,11 @@
+package com.example.homeservicespringmvc.entity.users;
+
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor
+public class Manager extends Person{
+
+}
