29dba5f0894d97c04048ddea552fb13b5c37a123
Arashap4 <arashmohamadi.developer@gmail.com>
2023-01-04 18:33:38 +0330

    create order status

---
+++ src/main/java/com/example/homeservicespringmvc/entity/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.example.homeservicespringmvc.entity.enums;
+
+public enum OrderStatus {
+    PENDING_FOR_SPECIALIST_SUGGESTION,
+    PENDING_FOR_SPECIALIST_SELECTION,
+    PENDING_FOR_SPECIALIST_COMING,
+    STARTED,
+    DONE,
+    PAID
+}
