3d6317dbb038626959c458c0fd0a9304a81b627c
Arashap4 <arashmohamadi.developer@gmail.com>
2023-01-04 19:06:29 +0330

    edit Suggestion class

--- src/main/java/com/example/homeservicespringmvc/entity/capability/Suggestion.java
+++ src/main/java/com/example/homeservicespringmvc/entity/capability/Suggestion.java
@@ -1,13 +1,16 @@
 package com.example.homeservicespringmvc.entity.capability;
 
 import com.example.homeservicespringmvc.entity.users.Specialist;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 
@@ -20,9 +23,14 @@ public class Suggestion extends BaseAbility {
     @CreationTimestamp
     private LocalDateTime creationDate;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startWork;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endWork;
+
+    @Transient
+    private Duration duration;
     @Positive(message = "price should positive")
     @NotNull
     private double price;
@@ -36,9 +44,10 @@ public class Suggestion extends BaseAbility {
     private Order order;
 
     @Builder
-    public Suggestion(LocalDateTime startWork, LocalDateTime endWork, double price) {
+    public Suggestion(LocalDateTime startWork, LocalDateTime endWork, Duration duration, double price) {
         this.startWork = startWork;
         this.endWork = endWork;
+        this.duration = duration;
         this.price = price;
     }
 }
