a56d15d4d49e0efe54f973d162f42cabef6796b7
Arashap4 <arashmohamadi.developer@gmail.com>
2023-01-12 21:22:06 +0330

    add file property

---
+++ src/test/java/com/example/homeservicespringmvc/HomeServiceSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeservicespringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
